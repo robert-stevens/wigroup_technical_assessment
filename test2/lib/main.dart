ecffe950c761ae02e995af141ba54afaee7fa75f
Robert Stevens <dev@uppercase.army>
2020-03-19 13:13:55 +0200

    test2 code cleanup

--- test2/lib/main.dart
+++ test2/lib/main.dart
@@ -1,29 +1,6 @@
-// import 'package:flutter/material.dart';
-// import 'package:test1/search_list.dart';
-
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Search',
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(
-//         brightness: Brightness.light,
-//         primarySwatch:Colors.yellow,
-//         primaryColor: Color(0xFFFFBB54),
-//         accentColor: Color(0xFFECEFF1),
-//       ),
-//       home: SearchList(),
-//     );
-//   }
-// }
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:test1/widgets/news_list.dart';
-// import 'package:test1/widgets/news_list.dart';
 
 void main() => runApp(App());
 
@@ -32,7 +9,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Networking',
       home: NewsList()
     );
